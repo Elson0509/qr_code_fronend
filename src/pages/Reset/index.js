68e0e91e0d4d8eae2b792df7249d38d83a13cebb
Elson Ramos <32194122+Elson0509@users.noreply.github.com>
2022-04-11 20:31:13 -0400

    adding _redirects file

---
+++ src/pages/Reset/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import api from '../../services/api'
+
+const Reset = props => {
+    console.log(props)
+    return (
+        <div>
+            teste
+        </div>
+    );
+};
+
+export default Reset;
