e0a327b17aadf2e79743f0983f49b81d2ae879d2
Elson Ramos <32194122+Elson0509@users.noreply.github.com>
2022-05-02 11:35:30 -0400

    adding log api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+console.log(process.env.REACT_APP_API_URL_PREFIX+process.env.REACT_APP_API_URL+process.env.REACT_APP_API_SERVER_URL)
+
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL_PREFIX+process.env.REACT_APP_API_URL+process.env.REACT_APP_API_SERVER_URL,
     timeout: 8000,
